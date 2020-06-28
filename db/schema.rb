ec0a46218637e0af4d14094483ee325cd2e72983
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-06-28 13:40:01 +0900

    edit/css

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200624055201) do
+ActiveRecord::Schema.define(version: 2020_06_24_055201) do
 
   create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.integer "user_id"
   end
 
   create_table "chats", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "room_id"
-    t.string   "message"
+    t.integer "user_id"
+    t.integer "room_id"
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_chats_on_room_id"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20200624055201) do
   end
 
   create_table "user_rooms", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "room_id"
+    t.integer "user_id"
+    t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_user_rooms_on_room_id"
@@ -46,21 +46,21 @@ ActiveRecord::Schema.define(version: 20200624055201) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "name"
-    t.text     "introduction"
-    t.string   "profile_image_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
