b6b10eb266ef825ea4a04b523d0d6acf66b3e3b1
vagrant <vagrant@localhost.localdomain>
2020-06-29 16:08:00 +0000

    add/acts-as-taggable-on

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,7 +43,7 @@ class BooksController < ApplicationController
 
   private
     def book_params
-      params.require(:book).permit(:title, :body)
+      params.require(:book).permit(:title, :body, :tag_list)
     end
 
     def screen_user(book)
